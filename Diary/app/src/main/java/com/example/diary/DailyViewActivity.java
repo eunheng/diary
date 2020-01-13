2b6e63df6db61e3eda9d8a962db95673fc8e0727
eunheng <2eunheng@gmail.com>
2020-01-13 19:05:30 +0900

    서버와 연결 전

--- Diary/app/src/main/java/com/example/diary/DailyViewActivity.java
+++ Diary/app/src/main/java/com/example/diary/DailyViewActivity.java
@@ -1,40 +1,321 @@
 package com.example.diary;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-public class DailyViewActivity extends Activity implements View.OnClickListener {
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+
+import java.util.Calendar;
+
+public class DailyViewActivity extends Activity implements View.OnClickListener, View.OnTouchListener{
 
     //widget
     private ImageButton btn_convert;
+    private GestureDetector gestureScanner = null;
+    private Button btn_datemove, btn_done, btn_edit;
+    private DatePicker dp_fixedPicker;
+    private TextView tv_day;
+    private EditText et_diary;
+    private Spinner sp_question;
+
+    private DiaryAdapter diaryAdapter;
+
+    //val
+    private static final int SWIPE_MIN_DISTANCE = 120;
+    private static final int SWIPE_MAX_OFF_PATH = 250;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+    private int year,month,day; //현재 보여주고 있는 일기의 날짜
+    private CalendarDay mCalendarDay;
+    private int dpYear, dpMonth, dpDay;
+    private Calendar calendar;
+    private String[] questions, diarys;
+    private String question, diary;
+    private ArrayAdapter<String> questionAdpater;
+    private String[] questionList;
+    private int btnDone=8,btnEdit=0;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dailyview);
         init();
+        showDiary();
+        checkDatePicker();
+        //swipe();
     }
 
     private void init() {
         btn_convert = (ImageButton) findViewById(R.id.btn_convertView);
-        btn_convert.setOnClickListener(this);       //뷰 전환 버튼 클릭 이벤트
+        btn_datemove = (Button) findViewById(R.id.btn_datemove);
+        btn_done = (Button) findViewById(R.id.btn_done);
+        btn_edit = (Button) findViewById(R.id.btn_edit);
+        //btn_question = (Button) findViewById(R.id.btn_question);
+        dp_fixedPicker = (DatePicker) findViewById(R.id.dp_fixedPicker);
+        tv_day = (TextView) findViewById(R.id.tv_day);
+        et_diary = (EditText) findViewById(R.id.et_diary);
+        sp_question = (Spinner) findViewById(R.id.sp_question);
+        //Realm.init(this);
+        mCalendarDay = CalendarDay.from(Calendar.getInstance());
+        btn_convert.setOnClickListener(this);
+        btn_datemove.setOnClickListener(this);
+        btn_done.setOnClickListener(this);
+        btn_edit.setOnClickListener(this);
+        //btn_question.setOnClickListener(this);
+        et_diary.setOnTouchListener(this);
+        questionList = getResources().getStringArray(R.array.defalt_questrons);
+        questionAdpater = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,questionList){
+            @Override
+            public int getCount() {
+                return super.getCount()-1;
+            }
+
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                return super.getView(position, convertView, parent);
+            }
+        };
+        sp_question.setAdapter(questionAdpater);
+        sp_question.setSelection(0);
+        sp_question.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                question = questionList[i];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
+
+    public static Intent intentToDaily(Context context, int year, int month, int day, String[] questions , String[] diarys) {
+        Intent intent = new Intent(context, DailyViewActivity.class);
+        intent.putExtra("year", year);
+        intent.putExtra("month", month);
+        intent.putExtra("day", day);
+        intent.putExtra("questions", questions);
+        intent.putExtra("diarys",diarys);
+        return intent;
+    }
+
+    private void intentToMonthly() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("year", year);
+        intent.putExtra("month", month);
+        intent.putExtra("day", day);
+        intent.putExtra("questions", questions);
+        intent.putExtra("diarys",diarys);
+        startActivity(intent);
+        finish();
+    }
+
+    private void showDiary() {
+        Intent intent = getIntent();
+        if (intent != null) {
+            calendar = Calendar.getInstance();
+            year = intent.getIntExtra("year", calendar.get(Calendar.YEAR));
+            month = intent.getIntExtra("month", calendar.get(Calendar.MONTH));
+            day = intent.getIntExtra("day", calendar.get(Calendar.DATE));
+            dpYear = year;
+            dpMonth= month;
+            dpDay = day;
+            calendar.set(year,month,day);
+            dp_fixedPicker.updateDate(year,month,year);
+            questions = intent.getStringArrayExtra("questions");
+            diarys = intent.getStringArrayExtra("diarys");
+            tv_day.setText(String.valueOf(year)+"년 "+String.valueOf(month+1)+"월 "+String.valueOf(day)+"일");
+            if (!TextUtils.isEmpty(diarys[day-1]))
+            {
+                et_diary.setText(diarys[day-1]);
+                for (int i=0; i<questionList.length; i++)
+                    if (questionList[i].equals(questions[day-1]))
+                        sp_question.setSelection(i);
+            }
+
+        }
+
+    }
+
+    //상단 스피너 날짜 이동값 저장(초기값 : 먼슬리에서 받아온 날짜)
+    private void checkDatePicker(){
+        dp_fixedPicker.init(year, month,day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int yy, int mm, int dd) {
+                dpYear = yy;
+                dpMonth = mm;
+                dpDay = dd;
+            }
+        });
+    }
+
+    //EditText 영역에 대한 swipe gesture 인식
+    private void swipe(){
+
+    }
+
+    private void setCalendar(Calendar calendar){
+        mCalendarDay.from(calendar);
+        year = mCalendarDay.getYear();
+        month = mCalendarDay.getMonth();
+        day = mCalendarDay.getDay();
+        updateDiary(year,month,day);
+    }
+
+    private void updateDiary(int year, int month, int day){
+        if (!((year==this.year)||(month==this.month)))
+        {
+            diaryAdapter = new DiaryAdapter(year,month,day);
+            questions = diaryAdapter.questions;
+            diarys = diaryAdapter.diarys;
+            question = diaryAdapter.questions[day-1];
+            diary = diaryAdapter.diarys[day-1];
+        }else {
+            question = questions[day-1];
+            diary = diarys[day-1];
+        }
+        tv_day.setText(String.valueOf(year)+"년 "+String.valueOf(month+1)+"월 "+String.valueOf(day)+"일");
+        if (!TextUtils.isEmpty(diary))
+        {
+            et_diary.setText(diary);
+            for (int i=0; i<questionList.length; i++)
+                if (questionList[i].equals(questions[day-1]))
+                    sp_question.setSelection(i);
+        }
+
+
+    }
+
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.btn_convertView:
-                Intent intent = new Intent(this, MainActivity.class);
-                startActivity(intent);
-                finish();
+            case R.id.btn_convertView:  //뷰 전환
+                intentToMonthly();
+                break;
+            case R.id.btn_datemove: //피커에서 받아온 값으로 일기 업데이트
+                calendar.set(dpYear,dpMonth,dpDay);
+                updateDiary(dpYear,dpMonth,dpDay);
+                break;
+            case R.id.btn_done: //메모 저장
+                /*final Diary diary = new Diary();
+                diary.setYear(year);
+                diary.setMonth(month);
+                diary.setDay(day);
+                diary.setQuestion(question);
+                diary.setText(et_diary.getText().toString());*/
+                et_diary.setFocusableInTouchMode(false);
+                et_diary.setFocusable(false);
+                et_diary.setClickable(false);
+                //sp_question.setEnabled(false);
+                btn_done.setEnabled(false);
+                btn_done.setTextColor(getResources().getColor(R.color.white));
+                btn_edit.setEnabled(true);
+                btn_edit.setTextColor(getResources().getColor(R.color.darkGray));
+
+                /** TODO 서버로 처음 저장
+                Realm realm = Realm.getDefaultInstance();
+                realm.executeTransaction(new Realm.Transaction(){
+
+                    @Override
+                    public void execute(Realm realm) {
+                        realm.copyToRealm(diary);
+                    }
+                });
+                realm.beginTransaction();
+                realm.copyToRealm(diary);
+                realm.commitTransaction();
+                realm.close();
+                finish();**/
+                break;
+            case R.id.btn_edit:
+                /** TODO 서버로 수정 값 보내기 **/
+                et_diary.setFocusableInTouchMode(true);
+                et_diary.setFocusable(true);
+                et_diary.setClickable(true);
+                //sp_question.setEnabled(true);
+                btn_edit.setEnabled(false);
+                btn_edit.setTextColor(getResources().getColor(R.color.white));
+                btn_done.setEnabled(true);
+                btn_done.setTextColor(getResources().getColor(R.color.darkGray));
                 break;
             default:
                 break;
         }
+    }
+
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        //swipe();
+        gestureScanner = new GestureDetector(this, new GestureDetector.OnGestureListener() {
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                try {
+                    if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+                        return false;
+
+                    // right to left swipe(next)
+                    if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                        //다음내용 갱신하기
+                        calendar.add(Calendar.DATE,1);
+                        //setCalendar(calendar);
+
+                    }
+                    // left to right swipe(prev)
+                    else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                        //이전내용 갱신하기
+                        calendar.add(Calendar.DATE,-1);
+                        //setCalendar(calendar);
+                    }
+                } catch (Exception e) {
+
+                }
+                return true;
+            }
+
+            public boolean onDown(MotionEvent motionEvent) {
+                return false;
+            }
+
+            public void onShowPress(MotionEvent motionEvent) {
+
+            }
+
+            public boolean onSingleTapUp(MotionEvent motionEvent) {
+                return false;
+            }
+
+            public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                return false;
+            }
+
+            public void onLongPress(MotionEvent motionEvent) {
+
+            }
+        });
+        gestureScanner.onTouchEvent(motionEvent);
+        setCalendar(calendar);
+        //et_diary.setClickable(true);
+        return true;
     }
 }
--- Diary/app/src/main/java/com/example/diary/MainActivity.java
+++ Diary/app/src/main/java/com/example/diary/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.diary;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.diary.decorator.EventDecorator;
 import com.example.diary.decorator.SaturdayDecorator;
@@ -27,31 +27,40 @@ import java.util.concurrent.Executors;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, OnDateSelectedListener, OnMonthChangedListener {
 
     //widget
-    private ImageButton btn_convert;
+    private ImageButton btn_convert,btn_setting;
+    private DatePicker dp_fixedPicker;
+    private Button btn_datemove;
+
     private MaterialCalendarView calendarView;
+    private DiaryAdapter diaryAdapter;
 
 
     //val
-    final CalendarDay instance = CalendarDay.today();
+    final CalendarDay todayDate = CalendarDay.today();
+    private int todayYear = todayDate.getYear(), todayMonth = todayDate.getMonth(), todayDay = todayDate.getDay();    //오늘 날짜
+    private CalendarDay mCalendarDay;
+    private int dpYear=todayYear,dpMonth=todayMonth,dpDay=todayDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-        checkedDay();
-
-
+        dailyToMonthly(); //처음 뷰 띄울 때 오늘을 기준으로 일기있는 날 체크, 인텐트 받아오면 일기 보던 날의 달로 체크
+        checkDatePicker();
     }
 
     private void init() {
         btn_convert = (ImageButton) findViewById(R.id.btn_convertView);
+        btn_setting = (ImageButton) findViewById(R.id.btn_setting);
         calendarView = (MaterialCalendarView) findViewById(R.id.calendarView);
-
+        dp_fixedPicker = (DatePicker) findViewById(R.id.dp_fixedPicker);
+        btn_datemove = (Button) findViewById(R.id.btn_datemove);
         btn_convert.setOnClickListener(this);       //뷰 전환 버튼 클릭 이벤트
+        btn_setting.setOnClickListener(this);
+        btn_datemove.setOnClickListener(this);
         calendarView.setOnDateChangedListener(this);    //날짜 클릭 이벤트
         calendarView.setOnMonthChangedListener(this);   //달 변화 이벤트
-        calendarView.setSelectedDate(instance);  //오늘 날짜를 기본으로 보여줌
         calendarView.addDecorators(
                 new SundayDecorator(),
                 new SaturdayDecorator()
@@ -59,13 +68,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    private void checkedDay(){
-        //TODO 일기가 저장된 날짜 받아오기
+    private void dailyToMonthly(){
+        Intent intent = getIntent();
+        if(intent != null){
+            dpYear = intent.getIntExtra("year", todayYear);
+            dpMonth = intent.getIntExtra("month", todayMonth);
+            dpDay = intent.getIntExtra("day", todayDay);
+        }
+        mCalendarDay = CalendarDay.from(dpYear,dpMonth,dpDay);
+        calendarView.setSelectedDate(mCalendarDay);
+        calendarView.setCurrentDate(mCalendarDay);
+        checkedDay(dpYear,dpMonth,dpDay);
+    }
+
+    //상단 스피너 날짜 이동값 저장
+    private void checkDatePicker(){
+        dp_fixedPicker.init(dpYear, dpMonth, dpDay , new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int yy, int mm, int dd) {
+                dpYear = yy;
+                dpMonth = mm;
+                dpDay = dd;
+            }
+        });
+    }
+
+    private void moveDiary(int year, int month, int day){
+        diaryAdapter = new DiaryAdapter(year,month,day);
+        startActivity(DailyViewActivity.intentToDaily(this,year,month,day,diaryAdapter.questions,diaryAdapter.diarys));
+        finish();
+    }
+
+    //일기 저장된 날 체크
+    private void checkedDay(int year, int month, int day){
+        diaryAdapter = new DiaryAdapter(year,month,day);
         String[] result = {"2020,01,18","2020,01,22","2020,01,04","2020,01,29"};
+        //new ApiSimulator(diaryAdapter.checkedDiarys).executeOnExecutor(Executors.newSingleThreadExecutor());
         new ApiSimulator(result).executeOnExecutor(Executors.newSingleThreadExecutor());
 
     }
-
 
     //달력에 dot 표시
     private class ApiSimulator extends AsyncTask<Void, Void, List<CalendarDay>> {
@@ -92,13 +133,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             //string 문자열인 Time_Result 을 받아와서 ,를 기준으로짜르고 string을 int 로 변환
             for(int i = 0 ; i < Time_Result.length ; i ++){
                 CalendarDay day = CalendarDay.from(calendar);
-                String[] time = Time_Result[i].split(",");
-                int year = Integer.parseInt(time[0]);
-                int month = Integer.parseInt(time[1]);
-                int dayy = Integer.parseInt(time[2]);
-
-                dates.add(day);
-                calendar.set(year,month-1,dayy);
+                if(!Time_Result[i].equals("NULL")){
+                    String[] time = Time_Result[i].split(",");
+                    int year = Integer.parseInt(time[0]);
+                    int month = Integer.parseInt(time[1]);
+                    int dayy = Integer.parseInt(time[2]);
+                    dates.add(day);
+                    calendar.set(year,month-1,dayy);
+                }
             }
 
             return dates;
@@ -119,8 +161,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.btn_convertView:
-                Intent intent = new Intent(this, DailyViewActivity.class);
+            case R.id.btn_convertView:  //무조건 오늘날짜 일기로
+                moveDiary(todayYear,todayMonth,todayDay);
+                break;
+            case R.id.btn_datemove: //피커에서 받아온 값으로 캘린더 이동, 일기있는 날 체크
+                mCalendarDay = CalendarDay.from(dpYear,dpMonth,dpDay);
+                calendarView.setCurrentDate(mCalendarDay);
+                calendarView.setSelectedDate(mCalendarDay);
+                checkedDay(dpYear,dpMonth,dpDay);
+                break;
+            case R.id.btn_setting: //피커에서 받아온 값으로 일기 업데이트
+                Intent intent = new Intent(this, SettingActivity.class);
+                //TODO 유저네임? intent.putExtra("year", year);
                 startActivity(intent);
                 finish();
                 break;
@@ -131,11 +183,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
-
+        //날짜 클릭 이벤트(캘린더에서 선택된 날짜 데일리뷰로 이동)
+        moveDiary(date.getYear(),date.getMonth(),date.getDay());
     }
 
     @Override
     public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
-
+        //달 변화 이벤트(일기있는 날 체크)
+        checkedDay(date.getYear(),date.getMonth(),date.getDay());
     }
 }
